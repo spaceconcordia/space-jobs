716ec304b64a98d94e32d287d4fb90c8ac177ad2
Ty Boer <ty@spaceconcordia.ca>
2013-12-15 20:54:24 -0500

    Reading value of ina219 via /sys/bus files; ina2xx.ko must be loaded and instantiated

--- read-pwr-ina219/src/main.cpp
+++ read-pwr-ina219/src/main.cpp
@@ -24,61 +24,48 @@ int main()
   Priority logPriority = NOTICE;
   string processName,logMsgVal;
   processName = "telemetryPwrIna219";
-  logMsgVal = "Test log payload string";
+  //logMsgVal = "Test log payload string";
 
-  // variables needed to retrieve ina status
   // *** note: for now we assume the ina2xx driver has already been instantiated 
   // *** ina219 is hardwired to: 
-  //   i2c bus: 1 
-  //   bus addr: 0x40
-  //   shunt resistor: 10mOhm
-  // *** ina219 internal register address table
-  // addr(hex) name
-  //  00h      Config Reg
-  //  01h      Shunt Voltage Reg
-  //  02h      Bus Voltage Reg
-  //  03h      Power Reg
-  //  04h      Current Reg
-  //  05h      Calibration Reg
+  // The data values can be read from the dev system if the existing module is loaded
+  // and instantiated
+  // DEV_FILES:
+  //  /sys/bus/i2c/devices/1-0040/curr1_input  :: current through Rsens
+  //  /sys/bus/i2c/devices/1-0040/in0_input    :: voltage across Rsens
+  //  /sys/bus/i2c/devices/1-0040/in1_input    :: voltage from Bus to Gnd
+  //  /sys/bus/i2c/devices/1-0040/power1_input :: power through Rsens
 
-  //FILE* i2cDevFile;
-  int i2cDevFile;
-  int addr = 0x40;
-  __u8 tgtRegister = 0x02;
-  __s32 res;
-  char buf[10];
-  memset(buf, 0, sizeof(buf));
+  FILE* inaSysFile;
+  char readBuf[10];
 
-  // open i2c bus
-  i2cDevFile=open("/dev/i2c-1",O_RDWR);
-  if (i2cDevFile<0 ) {
-    cout << "Failed to open i2c-1" << endl;
-    exit(1);
-  }
-  // tell i2c bus what device address to address
-  if (ioctl(i2cDevFile, I2C_SLAVE, addr) < 0) {
-    /* ERROR HANDLING; you can check errno to see what went wrong */
-    cout << "Failed to set addr 0x40" << endl;
-    exit(2);
-  }
+  inaSysFile=fopen("sys/bus/i2c/devices/1-0040/in1_input","r");
+  fgets(readBuf,10,inaSysFile);
+  fclose(inaSysFile);
+  logMsgVal.append("bus_v: ");
+  logMsgVal.append(readBuf);
+  memset(readBuf, 0, sizeof(readBuf));
 
-  buf[0]=tgtRegister;
-  res = write(i2cDevFile,buf,1); 
-  buf[0]=0x00;
-  res = read(i2cDevFile,buf,1);
-  if (res==1) {  
-    cout << "byte read success:" << buf[0] << endl; 
-  }
+  inaSysFile=fopen("sys/bus/i2c/devices/1-0040/in0_input","r");
+  fgets(readBuf,10,inaSysFile);
+  fclose(inaSysFile);
+  logMsgVal.append("shunt_v: ");
+  logMsgVal.append(readBuf);
+  memset(readBuf, 0, sizeof(readBuf));
 
-  /* Using SMBus commands */
-  //res = i2c_smbus_read_word_data(i2cDevFile, tgtRegister);
-  //if (res < 0) {
-  //  /* ERROR HANDLING: i2c transaction failed */
-  //} else {
-  //  cout << res << endl ;/* res contains the read word */
-  //}
+  inaSysFile=fopen("sys/bus/i2c/devices/1-0040/curr1_input","r");
+  fgets(readBuf,10,inaSysFile);
+  fclose(inaSysFile);
+  logMsgVal.append("shunt_i: ");
+  logMsgVal.append(readBuf);
+  memset(readBuf, 0, sizeof(readBuf));
 
-  close(i2cDevFile);
+  inaSysFile=fopen("sys/bus/i2c/devices/1-0040/power1_input","r");
+  fgets(readBuf,10,inaSysFile);
+  fclose(inaSysFile);
+  logMsgVal.append("power: ");
+  logMsgVal.append(readBuf);
+  memset(readBuf, 0, sizeof(readBuf));
 
   // write to log via shakespeare
   tgtLog=fopen("/tmp/telemetryPowerLog","a");

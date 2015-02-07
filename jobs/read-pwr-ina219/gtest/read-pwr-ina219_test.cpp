59716fe0076f755b222f4d6be93eba11f62a363c
Austin Hubbell <austin.hubbell@gmail.com>
2015-02-07 16:52:11 -0500

    Added tests, update Gtest makefile to match job template. Added error codes to main cpp file.

--- jobs/read-pwr-ina219/gtest/read-pwr-ina219_test.cpp
+++ jobs/read-pwr-ina219/gtest/read-pwr-ina219_test.cpp
@@ -6,34 +6,88 @@ class Read_Pwr_Ina219_Test : public ::testing::Test
         virtual void SetUp() { }
         int z=0;
 };
-//++++++++++++++++++++++++++++++++++++++logToShakespeare() Testing++++++++++++++++++++++++++++++++++++++++
 
-// The same process that leads to the final char array (timeAndPeakData) for logging is repeated here,
-// assuming proper data sent to binaryPeakMagData and detecionTime, then the logToShakespeare() function is called with
-// to log
-TEST_F(Read_Pwr_Ina219_Test, logCorrectDataNoticeShakespeare)
+//++++++++++++++++++++++++++++++++++++++readDevice() Testing++++++++++++++++++++++++++++++++++++++++
+
+// Test that the readDevice function returns success when passed the correct pPath and "curr1_input" device
+TEST_F(Read_Pwr_Ina219_Test, readFromCurr1)
 {
-    clock_t start_time = clock();
-    //The expected data from I2C should be a 12-bit char array
-    char binaryPeakMagData[12 + 1] = {0,0,0,0,1,0,1,0,1,0,1,0};
-    char detectionTimeData[10];
-    //Determine etermine elapsed time for detection
-    clock_t elapsed_time = clock() - start_time;
+    /******** /sys/bus/i2c/devices/1-0040/curr1_input  :: current through Rsens ********/
+    int readDeviceReturn = readDevice(pPath, "/curr1_input");
 
-    //Store peak detection time to temporary variable
-    float detectionTime = ((float)elapsed_time/CLOCKS_PER_SEC);
-    sprintf(detectionTimeData, "%f", detectionTime);
-
-    //Log peak magnitude and detection times using Shakspeare
-    char *temp = strcat(detectionTimeData, " ");
-    char *timeAndPeakData = strcat(temp, binaryPeakMagData);
-
-    int logReturn = logToShakespeare(timeAndPeakData, "NOTICE");
     ASSERT_EQ(
         CS1_SUCCESS, // expected
-        logReturn // actual
+        readDeviceReturn // actual
     );
 }
 
+// Test that the readDevice function returns success when passed the correct pPath and "in0_input" device
+TEST_F(Read_Pwr_Ina219_Test, readFromIn0)
+{
+    /******** /sys/bus/i2c/devices/1-0040/in0_input    :: voltage across Rsens ********/
+    int readDeviceReturn = readDevice(pPath, "/in0_input");
+
+    ASSERT_EQ(
+        CS1_SUCCESS, // expected
+        readDeviceReturn // actual
+    );
+}
+
+// Test that the readDevice function returns success when passed the correct pPath and "in1_input" device
+TEST_F(Read_Pwr_Ina219_Test, readFromIn1)
+{
+    /******** /sys/bus/i2c/devices/1-0040/in1_input    :: voltage from Bus to Gnd ********/
+    int readDeviceReturn = readDevice(pPath, "/in1_input");
+
+    ASSERT_EQ(
+        CS1_SUCCESS, // expected
+        readDeviceReturn // actual
+    );
+}
+
+// Test that the readDevice function returns success when passed the correct pPath and "power1_input" device
+TEST_F(Read_Pwr_Ina219_Test, readFromPower1)
+{
+    /********  /sys/bus/i2c/devices/1-0040/power1_input :: power through Rsens ********/
+    int readDeviceReturn = readDevice(pPath, "/power1_input");
+
+    ASSERT_EQ(
+        CS1_SUCCESS, // expected
+        readDeviceReturn // actual
+    );
+}
+
+// Test that the readDevice function returns a null path error when passed a null path
+TEST_F(Read_Pwr_Ina219_Test, readFromNullPath)
+{
+    int readDeviceReturn = readDevice(NULL, "/curr1_input");
+
+    ASSERT_EQ(
+        NULL_PATH_ERROR, // expected
+        readDeviceReturn // actual
+    );
+}
+
+// Test that the readDevice function returns a null device error when passed a null device
+TEST_F(Read_Pwr_Ina219_Test, readFromNullDevice)
+{
+    int readDeviceReturn = readDevice(pPath, NULL);
+
+    ASSERT_EQ(
+        NULL_DEVICENAME_ERROR, // expected
+        readDeviceReturn // actual
+    );
+}
+
+// Test that the readDevice function returns a null path error when passed a null path and null device
+TEST_F(Read_Pwr_Ina219_Test, readFromNullPathNullDevice)
+{
+    int readDeviceReturn = readDevice(NULL, NULL);
+
+    ASSERT_EQ(
+        NULL_PATH_ERROR, // expected
+        readDeviceReturn // actual
+    );
+}
 
 //---------------------------------------------------------------------------------------------------------
--- jobs/read-pwr-ina219/src/read-pwr-ina219.cpp
+++ jobs/read-pwr-ina219/src/read-pwr-ina219.cpp
@@ -14,6 +14,10 @@
 #define PROCESS_PATH "INA2XXPATH"
 #define LOG_PATH "/home/logs"
 
+//Error codes
+#define NULL_PATH_ERROR 2;
+#define NULL_DEVICENAME_ERROR 3;
+
 uint8_t process_id = CS1_PWR_INA219;
 const string process = cs1_systems[process_id];
 
@@ -26,7 +30,6 @@ int main()
 {
 	int exitStatus=0;
   char* pPath = getenv(PROCESS_PATH);
-
 
 
  	/******** /sys/bus/i2c/devices/1-0040/curr1_input  :: current through Rsens ********/
@@ -50,6 +53,12 @@ int main()
 }
 
 int readDevice (char* pPath, const char* deviceName) {
+	if(pPath == NULL)
+		return NULL_PATH_ERROR;
+
+	if (deviceName == NULL)
+		return NULL_DEVICENAME_ERROR;
+
 	char result[100];
 	strcpy(result,pPath); // copy string one into the result.
 	strcat(result,deviceName);
@@ -59,84 +68,3 @@ int readDevice (char* pPath, const char* deviceName) {
 
 	return CS1_SUCCESS;
 }
-
-/*
-  // main function variables
-  int exitStatus=0;
-
-  // variables needed to call shakespeare log function
-  string logMsgVal;
-
-  // *** note: for now we assume the ina2xx driver has already been instantiated
-  // *** ina219 is hardwired to:
-  //  bus: i2c-1
-  //  address: 0x40
-  // The data values can be read from the dev system if the existing module is loaded
-  // and instantiated
-  // DEV_FILES:
-  //  /sys/bus/i2c/devices/1-0040/curr1_input  :: current through Rsens
-  //  /sys/bus/i2c/devices/1-0040/in0_input    :: voltage across Rsens
-  //  /sys/bus/i2c/devices/1-0040/in1_input    :: voltage from Bus to Gnd
-  //  /sys/bus/i2c/devices/1-0040/power1_input :: power through Rsens
-
-  FILE* inaSysFile;
-  char readBuf[10];
-  size_t lastLen=0;
-
-  inaSysFile=fopen("/sys/bus/i2c/devices/1-0040/in1_input","r");
-  if (inaSysFile!=NULL) {
-    fgets(readBuf,10,inaSysFile);
-    fclose(inaSysFile);
-  } else {
-    strncpy(readBuf,"readfail\n",9);
-  }
-  logMsgVal.append("bus_v: ");
-  lastLen=logMsgVal.length()+1;
-  logMsgVal.append(readBuf);
-  logMsgVal.erase(logMsgVal.find("\n",lastLen),string::npos);
-  logMsgVal.append(" :: ");
-  memset(readBuf, 0, sizeof(readBuf));
-
-  inaSysFile=fopen("/sys/bus/i2c/devices/1-0040/in0_input","r");
-  if (inaSysFile!=NULL) {
-    fgets(readBuf,10,inaSysFile);
-    fclose(inaSysFile);
-  } else {
-    strncpy(readBuf,"readfail\n",9);
-  }
-  logMsgVal.append("shunt_v: ");
-  lastLen=logMsgVal.length()+1;
-  logMsgVal.append(readBuf);
-  logMsgVal.erase(logMsgVal.find("\n",lastLen),string::npos);
-  logMsgVal.append(" :: ");
-  memset(readBuf, 0, sizeof(readBuf));
-
-  inaSysFile=fopen("/sys/bus/i2c/devices/1-0040/curr1_input","r");
-  if (inaSysFile!=NULL) {
-    fgets(readBuf,10,inaSysFile);
-    fclose(inaSysFile);
-  } else {
-    strncpy(readBuf,"readfail\n",9);
-  }
-  logMsgVal.append("shunt_i: ");
-  lastLen=logMsgVal.length()+1;
-  logMsgVal.append(readBuf);
-  logMsgVal.erase(logMsgVal.find("\n",lastLen),string::npos);
-  logMsgVal.append(" :: ");
-  memset(readBuf, 0, sizeof(readBuf));
-
-  inaSysFile=fopen("/sys/bus/i2c/devices/1-0040/power1_input","r");
-  if (inaSysFile!=NULL) {
-    fgets(readBuf,10,inaSysFile);
-    fclose(inaSysFile);
-  } else {
-    strncpy(readBuf,"readfail\n",9);
-  }
-  logMsgVal.append("power: ");
-  logMsgVal.append(readBuf);
-  logMsgVal.erase(logMsgVal.find("\n",lastLen),string::npos);
-  memset(readBuf, 0, sizeof(readBuf));
-
-  Shakespeare::log(Shakespeare::NOTICE,PROCESS,logMsgVal);
-
-  return exitStatus;*/
